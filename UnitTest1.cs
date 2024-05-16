5fc2c6008eb5ea1aec193ede6015d9b39cc7375e
gyozo-sz <gyozo.szabo@nixs.com>
2024-05-16 10:38:59 +0200

    PageObject implementation added

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,37 +1,20 @@
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
 using System.Xml.Serialization;
 
 namespace NUnit_practice
 {
-    public class SeleniumWebDriverTest
+    public class LoginPageTests
     {
         private IWebDriver webDriver;
-        private readonly Dictionary<string, By> locatorMap = new Dictionary<string, By>();
+        MyAccountPage myAccountPage;
+        DataConsentPage dataConsentPage;
+        readonly string URL = "https://practice.automationtesting.in/my-account/";
+        readonly string LostPasswordURL = "https://practice.automationtesting.in/my-account/lost-password/";
 
-        public SeleniumWebDriverTest() {
-            locatorMap = new Dictionary<string, By>()
-            {
-                { "GDRP_do_not_consent_button",             By.CssSelector("button.fc-cta-do-not-consent") },
-                { "search_bar",                             By.CssSelector("form#searchform input") },
-                { "search_result_product_cards",            By.XPath("//article[contains(@class, 'product')]")},
-                { "search_result_product_title",            By.XPath("//article[contains(@class, 'product')]//h2")},
-                { "search_result_product_link",             By.XPath("//article[contains(@class, 'product')]//a")},
-                { "on_sale_sticker",                        By.CssSelector("div.product > span.onsale")},
-                { "product_card_title",                     By.CssSelector("h1.product_title")},
-                { "product_card_price_span",                By.CssSelector("p.price span.amount")},
-                { "search_page_title",                      By.CssSelector("h1.page-title")},
-                {"add_to_cart_button",                      By.CssSelector("form.cart button")},
-                { "navbar_cart_button",                     By.CssSelector("nav a[class*=cart]")},
-                { "cart_product_name_table_element",        By.CssSelector("td.product-name")},
-                { "cart_product_total_price_table_element", By.CssSelector("td.product-subtotal span.amount")}
-        };
-            
-        }
-
-        public By ProductCardLocatorByProductName(string productName) => By.XPath($"//a[contains(text(), '{productName}')]");
-        public By RelatedProductLocatorByProductName(string relatedProductName) => 
-            By.XPath($"//div[contains(@class, 'related products')]//*[contains(text(), '{relatedProductName}')]");
+        readonly string validEmail = "some@email.com";
+        readonly string validPassword = ")3jf8BdjL2a$smm";
 
         [SetUp]
         public void Setup()
@@ -41,125 +24,97 @@ namespace NUnit_practice
             webDriver = new ChromeDriver(chromeOptions);
             webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
-            webDriver.Navigate().GoToUrl("https://practice.automationtesting.in/shop/");
-            WaitAndCloseGDPRPopup();
-            //IJavaScriptExecutor js = (IJavaScriptExecutor)webDriver;
-        }
+            webDriver.Navigate().GoToUrl(URL);
 
-        private IWebElement FindElementWithWait(By selectedBy, uint waitTime)
-        {
-            WebDriverWait wait = new(webDriver, TimeSpan.FromSeconds(waitTime));
-            return wait.Until(e => e.FindElement(selectedBy));
+            myAccountPage = new MyAccountPage();
+            dataConsentPage = new DataConsentPage();
+            PageFactory.InitElements(webDriver, myAccountPage);
+            PageFactory.InitElements(webDriver, dataConsentPage);
+
+            WaitAndCloseGDPRPopup();
         }
 
         private void WaitAndCloseGDPRPopup()
         {
-            webDriver.FindElement(locatorMap["GDRP_do_not_consent_button"]).Click();
-        }
-
-        private void SearchForPhrase(string searchPhrase)
-        {
-            IWebElement searchForm = webDriver.FindElement(locatorMap["search_bar"]);
-            searchForm.SendKeys(searchPhrase + "\n");
-        }
-
-        private void CheckThatResultsContainTheSearchPhrase(string searchPhrase)
-        {
-            IReadOnlyList<IWebElement> htmlProductCards = webDriver.FindElements(locatorMap["search_result_product_cards"]);
-
-            foreach (IWebElement htmlProductCard in htmlProductCards)
-            {
-                IReadOnlyList<IWebElement> productLinks = htmlProductCard.FindElements(locatorMap["search_result_product_link"]);
-                Assert.That(productLinks, Is.Not.Empty);
-                IWebElement productLink = productLinks[0];
-                Assert.That(productLink.GetAttribute("href"), Does.StartWith("https://"));
-
-                
-                IWebElement productName = htmlProductCard.FindElement(locatorMap["search_result_product_title"]);
-                Assert.That(productName.Text, Does.Contain(searchPhrase));
-            }
-        }
-
-        private void ClickProductInSearchResult(string productName)
-        {
-            string selectedProductName = "Thinking in HTML";
-            IWebElement selectedProductCard = webDriver.FindElement(ProductCardLocatorByProductName(selectedProductName));
-            Actions actions = new(webDriver);
-            actions.MoveToElement(selectedProductCard);
-            actions.Click();
-            actions.Perform();
-        }
-
-        private void CheckThatSaleStickerIsDisplayed()
-        {
-            Assert.That(webDriver.FindElements(locatorMap["on_sale_sticker"]), Is.Not.Empty);
-        }
-
-        private void CheckThatOldAndNewPricesAreDisplayed()
-        {
-            IReadOnlyCollection<IWebElement> productPrices = webDriver.FindElements(locatorMap["product_card_price_span"]);
-            Assert.That(productPrices.Count, Is.EqualTo(2));
-        }
-
-        private void NavigateToRelatedProduct(string relatedProductName)
-        {
-            IWebElement relatedProductCart = webDriver.FindElement(RelatedProductLocatorByProductName(relatedProductName));
-            Actions actions = new(webDriver);
-            actions.MoveToElement(relatedProductCart);
-            actions.Perform();
-            IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)webDriver;
-            relatedProductCart.Click();
-        }
-
-        private (string, string) GetProductNameAndPrice()
-        {
-            string titleDisplayedOnSite = webDriver.FindElement(locatorMap["product_card_title"]).Text;
-            string priceDisplayedOnSite = webDriver.FindElement(locatorMap["product_card_price_span"]).Text;
-            return (titleDisplayedOnSite, priceDisplayedOnSite);
-        }
-
-        private void AddProductToCart()
-        {
-            webDriver.FindElement(locatorMap["add_to_cart_button"]).Click();
-        }
-
-        private void NavigateToCart()
-        {
-            webDriver.FindElement(locatorMap["navbar_cart_button"]).Click();
-        }
-
-        private void CheckThatProductNameAndPriceMatch(string productNameOnCard, string productPriceOnCard)
-        {
-            Assert.That(webDriver.FindElement(locatorMap["cart_product_name_table_element"]).Text, Is.EqualTo(productNameOnCard));
-            Assert.That(webDriver.FindElement(locatorMap["cart_product_total_price_table_element"]).Text, Is.EqualTo(productPriceOnCard));
-        }
-
-        private void CheckThatTitleContainsSearchPhrase(string searchPhrase)
-        {
-            Assert.That(webDriver.FindElement(locatorMap["search_page_title"]).Text, Does.Contain(searchPhrase));
-            Assert.That(webDriver.Title, Does.Contain(searchPhrase));
+            dataConsentPage.RejectDataUse();
         }
 
         [Test]
-        public void ShoppingFlowE2E()
+        public void LoginUsernameFieldTest()
         {
-            string searchPhrase = "HTML";
-            SearchForPhrase(searchPhrase);
-            CheckThatTitleContainsSearchPhrase(searchPhrase);
-            CheckThatResultsContainTheSearchPhrase(searchPhrase);
+            string username = "some@username.com";
+            myAccountPage.EnterLoginUsername(username);
+            Assert.That(myAccountPage.GetLoginUsername(), Is.EqualTo(username));
+        }
 
-            ClickProductInSearchResult("Thinking in HTML");
-            CheckThatSaleStickerIsDisplayed();
-            CheckThatOldAndNewPricesAreDisplayed();
+        [Test]
+        public void LoginPasswordFieldTest()
+        {
+            string password = "pass#23@{a.";
+            myAccountPage.EnterLoginPassword(password);
+            Assert.That(myAccountPage.GetLoginPassword(), Is.EqualTo(password));
+        }
 
-            string html5WebAppDevelopmentTitle = "HTML5 WebApp Develpment";
-            NavigateToRelatedProduct(html5WebAppDevelopmentTitle);
+        [Test]
+        public void RememberMeTest()
+        {
+            bool isRememberMeActive = myAccountPage.IsRememberMeChecked();
+            myAccountPage.ClickRememberMeCheckbox();
+            Assert.That(myAccountPage.IsRememberMeChecked(), Is.EqualTo(!isRememberMeActive));
+            myAccountPage.ClickRememberMeCheckbox();
+            Assert.That(myAccountPage.IsRememberMeChecked(), Is.EqualTo(isRememberMeActive));
+        }
 
-            (string productNameOnCard, string productPriceOnCard) = GetProductNameAndPrice();
-            AddProductToCart();
+        [Test]
+        public void RegisterUsernameFieldTest()
+        {
+            string username = "some@username.com";
+            myAccountPage.EnterRegisterEmail(username);
+            Assert.That(myAccountPage.GetRegisterEmail(), Is.EqualTo(username));
+        }
 
-            NavigateToCart();
-            CheckThatProductNameAndPriceMatch(productNameOnCard, productPriceOnCard);
+        [Test]
+        public void RegisterPasswordFieldTest()
+        {
+            string password = "pass#23@{a.";
+            myAccountPage.EnterLoginPassword(password);
+            Assert.That(myAccountPage.GetLoginPassword(), Is.EqualTo(password));
+        }
+
+        [Test]
+        public void LostPasswordLinkTest()
+        {
+            myAccountPage.ClickLostPasswordLink();
+            Assert.That(webDriver.Url, Is.EqualTo(LostPasswordURL));
+        }
+
+        [Test]
+        public void LoginWithValidCredentials()
+        {
+            myAccountPage.LoginUser(validEmail, validPassword);
+
+            string username = validEmail.Split('@')[0];
+            Assert.That(myAccountPage.GetLogInGreeting(), Does.Contain(username));
+        }
+
+        [Test]
+        public void LoginWithInvalidEmail()
+        {
+            string invalidEmail = "a" + validEmail;
+            myAccountPage.LoginUser(invalidEmail, validPassword);
+
+            Assert.That(myAccountPage.GetErrorMessageText(), Does.Contain("Error"));
+            Assert.That(myAccountPage.GetErrorMessageText(), Does.Contain("email"));
+        }
+
+        [Test]
+        public void LoginWithInvalidPassword()
+        {
+            string invalidPassword = "a" + validPassword;
+            myAccountPage.LoginUser(validEmail, invalidPassword);
+
+            Assert.That(myAccountPage.GetErrorMessageText(), Does.Contain("Error"));
+            Assert.That(myAccountPage.GetErrorMessageText(), Does.Contain("password"));
         }
 
         [TearDown]
